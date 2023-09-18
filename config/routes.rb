f2234db9f04e2a4d6463d6d27a0050c9f1d8e3a9
Kyle Nichols <kyle.r.nichols@gmail.com>
2023-09-18 17:05:54 +0900

    fixed category migration error and re-tooled routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,15 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :new, :create]
+
   resources :shopping_carts, only: [:index, :show, :new, :create] do
     resources :cart_members, only: [:create]
-    resources :items, only: [:index, :show, :new, :create] do
-      resources :cart_items, only: [:create]
-    end
   end
 
-  resources :cart_items, only: [:update]
+  resources :items, only: [:index, :show, :new, :create] do
+    resources :cart_items, only: [:update, :create]
+  end
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
