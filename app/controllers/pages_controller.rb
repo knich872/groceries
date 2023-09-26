061f77695df1d9f1dd2757b22ecd4498c89f78a0
Kyle Nichols <kyle.r.nichols@gmail.com>
2023-09-26 21:26:26 +0900

    modified home page for whether or not user is signed in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def home
-    @shopping_carts = ShoppingCart.where(id: current_user.shopping_cart_ids)
+    if user_signed_in?
+      @shopping_carts = ShoppingCart.where(id: current_user.shopping_cart_ids)
+    end
   end
 end
