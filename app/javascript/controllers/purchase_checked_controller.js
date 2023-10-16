18d5d8cf68c9ea24acdbe2fd67f4f895ca9fc83a
Kyle Nichols <kyle.r.nichols@gmail.com>
2023-10-16 16:26:05 +0900

    check function returns array of urls for each checked item

--- app/javascript/controllers/purchase_checked_controller.js
+++ app/javascript/controllers/purchase_checked_controller.js
@@ -1,22 +1,36 @@
 import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="purchase-checked"
+let purchaseArray = [];
 export default class extends Controller {
   static values = {
     count: Number,
     url: String
   }
-  static targets = ["url"]
+  static targets = [""]
 
   connect() {
     console.log("hello from the purchase checked controller");
     console.log(this.countValue);
-    console.log(this.urlValues);
-    console.log(this.urlTargets);
+    // console.log(this.urlValues);
+    // console.log(this.urlTargets);
   }
 
+
   check(e) {
-    console.log(this.urlTargets);
+    let url = e.target.baseURI + e.target.dataset.purchaseCheckedUrlValue
+    console.log(url);
+    if (purchaseArray.includes(url)) {
+      purchaseArray = purchaseArray.filter(element => element !== url);
+    } else {
+      purchaseArray.push(url);
+    }
+    console.log(purchaseArray);
+    return purchaseArray
+  }
+
+  purchase() {
+
   }
 }
 
